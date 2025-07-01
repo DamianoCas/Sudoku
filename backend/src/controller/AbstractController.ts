7d09277923793d26c0b823111a65c92195a47a63
DamianoCas <damianocastellano2004@gmail.com>
2025-07-01 16:41:16 +0200

    added end statements for the errors

--- backend/src/controller/AbstractController.ts
+++ backend/src/controller/AbstractController.ts
@@ -5,15 +5,19 @@ export default abstract class AbstractController {
     protected internalError(response: Response, message?: string): void{
         response.status(500);
         response.json({ error: message != undefined ? message : 'internal server error' });
+        response.end();
+
     }
 
     protected badRequestError(response: Response, message?: string): void {
-        response.status(401);
+        response.status(400);
         response.json({ error: message != undefined ? message : 'client side error' });
+        response.end();
     }
 
     protected notFoundError(response: Response, message?: string): void {
         response.status(404);
         response.json({ error: message != undefined ? message : 'resource not found' });
+        response.end();    
     }
 }
