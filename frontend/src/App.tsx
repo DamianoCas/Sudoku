09b59a72a92cb6255162f54301e604f51ac9f687
DamianoCas <damianocastellano2004@gmail.com>
2025-07-01 14:33:22 +0200

    moved game logic into game component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-import { type sudokuSpecifics } from "./components/SudokuBoard"
-import {type TimerState as State, TimerState} from "./components/Timer"
+import { useState } from "react";
 import Header from "./components/Header";
 import GameComponent from "./components/GameComponent";
 import User, { type UserType } from "./components/UserComponent";
@@ -15,14 +13,8 @@ export type PageState = (typeof PageState)[keyof typeof PageState];
 
 
 function App() {
-  const [boardSpecifics, setBoardSpecifics] = useState<sudokuSpecifics | null>(null);
-  const [timerRunning, setTimerRunning] = useState<State>(TimerState.Stop);
   const [pageState, setPageState] = useState<PageState>(PageState.Game);
   const [user, setUser] = useState<UserType | null>(null);
-
-  const handleStartTimer = () => { setTimerRunning(TimerState.Start) }
-  const handleStopTimer = () => { setTimerRunning(TimerState.Stop) }
-  const handleResetTimer = () => { setTimerRunning(TimerState.Reset) }
 
   const handleUserPage = () => { setPageState(PageState.User) }
   const handleGamePage = () => { setPageState(PageState.Game) }
@@ -30,36 +22,6 @@ function App() {
 
   const handleUserChange = (newUser: UserType | null) => { setUser(newUser) }
 
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getBoardFromDB(289);
-        setBoardSpecifics(data);
-      } catch (err) {
-        console.error(err);
-      } 
-    };
-    
-    fetchData();
-  }, []);
-  
-  async function getBoardFromDB(index: number): Promise<sudokuSpecifics> {
-    try {
-      const response = await fetch(`/api/sudokuBoard/${index}`);
-      return await response.json();
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  }
-  
-  async function handleNewBoard(index: number) {
-    if (index === -1) index = Math.floor(Math.random() * (999))+1;
-
-    const board = await getBoardFromDB(index);
-    setBoardSpecifics(board);
-  }
   
   return (
     <div>
@@ -69,13 +31,7 @@ function App() {
       <main>
         {
           pageState == PageState.User ?  <User onUserChange={handleUserChange} user={user}/>
-          : pageState == PageState.Game ? <GameComponent 
-            boardSpecifics={boardSpecifics}
-            timerRunning={timerRunning}
-            handleStartTimer={handleStartTimer}
-            handleStopTimer={handleStopTimer}
-            handleResetTimer={handleResetTimer}
-            handleNewBoard={handleNewBoard} />
+          : pageState == PageState.Game ? <GameComponent user={user} handleUserChange={handleUserChange}/>
           : ("leader Board")
         }
       </main>
--- frontend/src/components/GameComponent.tsx
+++ frontend/src/components/GameComponent.tsx
@@ -1,41 +1,78 @@
+import { useEffect, useState } from "react";
+import Timer, {type TimerState as State, TimerState} from "./Timer";
 import SudokuBoard, { type sudokuSpecifics } from "./SudokuBoard"
-import Timer, { TimerState } from "./Timer"
+import type { UserType } from "./UserComponent";
 
-
-interface GameProps {
-  boardSpecifics?: sudokuSpecifics | null;
-  timerRunning: TimerState;
-  handleStartTimer: () => void;
-  handleStopTimer: () => void;
-  handleResetTimer: () => void;
-  handleNewBoard: (difficulty: number) => void;
+export interface GameType {
+  id: number;
+  easyMode: boolean;
+  board: sudokuSpecifics;
 }
 
-export default function GameComponent ({
-  boardSpecifics,
-  timerRunning,
-  handleStartTimer,
-  handleStopTimer,
-  handleResetTimer,
-  handleNewBoard
-}: GameProps) {
+interface GameProp {
+  user: UserType | null;
+  handleUserChange: (newUser: UserType | null) => void;
+}
+
+export default function GameComponent ( {user, handleUserChange}: GameProp) {
+  const [boardSpecifics, setBoardSpecifics] = useState<sudokuSpecifics | null>(null);
+  
+  const [timerRunning, setTimerRunning] = useState<State>(TimerState.Stop);
+  const [game, setGame] = useState<GameType | null>(null);
+  
+  const handleStartTimer = () => { setTimerRunning(TimerState.Start) }
+  const handleStopTimer = () => { setTimerRunning(TimerState.Stop) }
+  const handleResetTimer = () => { setTimerRunning(TimerState.Reset) }
+  
+  const handleGameChange = (newGame: GameType | null) => { setGame(newGame) }
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getBoardFromDB(289);
+        setBoardSpecifics(data);
+      } catch (err) {
+        console.error(err);
+      } 
+    };
+    
+    fetchData();
+  }, []);
+  
+  async function getBoardFromDB(index: number): Promise<sudokuSpecifics> {
+    try {
+      const response = await fetch(`/api/sudokuBoard/${index}`);
+      return await response.json();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+  
+  async function handleNewBoard(index: number) {
+    if (index === -1) index = Math.floor(Math.random() * (999))+1;
+    
+    const board = await getBoardFromDB(index);
+    setBoardSpecifics(board);
+  }
+  
   return (
     <div>
-        <div style={{ display: "flex", justifyContent: "space-between", alignSelf: "center"}} >
-            <h1>Id: {boardSpecifics?.id}</h1>
-            <Timer timerRunning={timerRunning}/>
-            <h1>Difficulty: {boardSpecifics?.difficulty}</h1>
-        </div>
-        
-        <div className="sudoku-container">
-        {boardSpecifics ? (
-            <SudokuBoard specifics={boardSpecifics} onStartTimer={handleStartTimer} onStopTimer={handleStopTimer} onResetTimer={handleResetTimer}/>
-        ) : (
-            <div>Loading board...</div>
-        )}
-        </div>
-        
-        <button onClick={() => handleNewBoard(-1)}>New Board</button>
+    <div style={{ display: "flex", justifyContent: "space-between", alignSelf: "center"}} >
+    <h1>Id: {boardSpecifics?.id}</h1>
+    <Timer timerRunning={timerRunning}/>
+    <h1>Difficulty: {boardSpecifics?.difficulty}</h1>
+    </div>
+    
+    <div className="sudoku-container">
+    {boardSpecifics ? (
+      <SudokuBoard specifics={boardSpecifics} onStartTimer={handleStartTimer} onStopTimer={handleStopTimer} onResetTimer={handleResetTimer}/>
+    ) : (
+      <div>Loading board...</div>
+    )}
+    </div>
+    
+    <button onClick={() => handleNewBoard(-1)}>New Board</button>
     </div>
   );
 };
